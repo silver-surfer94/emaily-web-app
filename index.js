40c31676ba2a31a43eb357178401ae8c6217ad1b
Alex Dete <alex@Korelogics-MacBook-Pro.local>
2018-02-08 15:31:20 +0000

    second deployment commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ name: 'Alex' });
+  res.send({ hi: 'There' });
 });
 
 const PORT = process.env.PORT || 5000;
