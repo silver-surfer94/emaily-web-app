11e2c2eef187eb459325e5651d019e876f7bbc2c
Alex Dete <alex@Korelogics-MacBook-Pro.local>
2018-02-14 15:48:19 +0000

    added client

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the profile ID
-          done(null, existingUser);
-        } else {
-          // make a new record
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
     }
   )
 );
